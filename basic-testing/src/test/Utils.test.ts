1b32bfec777e727431867a42d9248787af137e4c
Junhyeong-B <aowlr0513@gmail.com>
2023-03-16 22:27:53 +0900

    feat: getStringInfo 함수 및 테스트 추가 - 각 항목에 맞는 메서드 적용

--- basic-testing/src/test/Utils.test.ts
+++ basic-testing/src/test/Utils.test.ts
@@ -1,8 +1,39 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
-  test("should return uppercase", () => {
-    const result = toUpperCase("abc");
-    expect(result).toBe("ABC");
+  it("should return uppercase of valid string", () => {
+    // arrange
+    const suite = toUpperCase;
+    const expected = "ABC";
+
+    // act
+    const actual = suite("abc");
+
+    // assert
+    expect(actual).toBe(expected);
+  });
+
+  it("should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.upperCase).toBe("MY-STRING");
+
+    expect(actual.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    // prettier-ignore
+    expect(actual.characters).toEqual(["M", "y", "-", "S", "t", "r", "i", "n", "g"]);
+    expect(actual.characters).toContain("M");
+    // prettier-ignore
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"])
+    );
+
+    expect(actual.extraInfo).toEqual({});
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
