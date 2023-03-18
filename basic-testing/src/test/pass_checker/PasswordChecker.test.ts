2b932890b1d5341fed513a7e5224d10f13fc38a1
Junhyeong-B <aowlr0513@gmail.com>
2023-03-18 16:57:45 +0900

    feat: 기본 패스워드 체커 기능 및 테스트 추가

--- basic-testing/src/test/pass_checker/PasswordChecker.test.ts
+++ basic-testing/src/test/pass_checker/PasswordChecker.test.ts
@@ -6,7 +6,33 @@ describe("PasswordChecker test suite", () => {
     suite = new PasswordChecker();
   });
 
-  test("Should do nothing for the moment", () => {
-    suite.checkPassword();
+  test("Password with less than 8 chars is invalid", () => {
+    const actual = suite.checkPassword("1234567");
+    expect(actual).toBe(false);
+  });
+
+  test("Password with more than 8 chars is valid", () => {
+    const actual = suite.checkPassword("12345678Aa");
+    expect(actual).toBe(true);
+  });
+
+  test("Password with no upper case is invalid", () => {
+    const actual = suite.checkPassword("1234abcd");
+    expect(actual).toBe(false);
+  });
+
+  test("Password with upper case is valid", () => {
+    const actual = suite.checkPassword("1234ABcd");
+    expect(actual).toBe(true);
+  });
+
+  test("Password with no lower case is invalid", () => {
+    const actual = suite.checkPassword("1234ABCD");
+    expect(actual).toBe(false);
+  });
+
+  test("Password with lower case is valid", () => {
+    const actual = suite.checkPassword("1234abCD");
+    expect(actual).toBe(true);
   });
 });
